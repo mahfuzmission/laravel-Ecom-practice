55cf323db43783d22f5966df1cec0136c7f55913
mahfuz <mahfuzmission.aiub@gmail.com>
2018-08-02 18:18:26 +0600

    e-commerce practice....login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,17 +1,40 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index(){
-       // echo "okk";
         return view('admin_login');
     }
 
     public function show_dashboard(){
         return view('admin.dashboard');
+    }
+
+    public function dashboard(Request $request){
+        $admin_email = $request->admin_email;
+        $admin_password = md5($request->admin_password);
+        $result = DB::table('tbl_admin')
+            ->where('admin_email',$admin_email)
+            ->where('admin_password',$admin_password)
+            ->first();
+//        echo "<pre>";
+//        print_r($result);
+//        exit();
+
+        if($result){
+            Session::put('admin_name',$result->admin_name);
+            Session::put('admin_id',$result->admin_id);
+            return Redirect::to('/dashboard');
+        }
+        else{
+            Session::put('message','Email or Password Invalid');
+            return Redirect::to('/admin');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,4 @@ Route::get('/','HomeController@index');
 //backend controller
 Route::get('/admin','AdminController@index');
 Route::get('/dashboard','AdminController@show_dashboard');
+Route::post('/admin-dashboard','AdminController@dashboard');
