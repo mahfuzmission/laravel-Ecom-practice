6fd581672746feac3c832f4b51b353ed51780279
mahfuz <mahfuzmission.aiub@gmail.com>
2018-08-06 17:24:31 +0600

    practice dashboard part 1-testing

---
+++ app/Admin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev-05
+ * Date: 8/6/2018
+ * Time: 10:43 AM
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model{
+        //protected $fillable =['email',''];
+}
+
+
